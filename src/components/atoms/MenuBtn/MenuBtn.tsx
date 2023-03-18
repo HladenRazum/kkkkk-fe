3fd2d4f728339b0727ba843ae66ee4f7ac06a84b
KrasimirKostadinov <krasimir.kostadinov@modis.com>
2023-03-18 13:47:48 +0200

    added menu icon

--- src/components/atoms/MenuBtn/MenuBtn.tsx
+++ src/components/atoms/MenuBtn/MenuBtn.tsx
@@ -1,11 +1,45 @@
+import { useRef } from "react";
+import "./menu-btn.scss";
+
 type MenuBtnProps = {
    onClick: () => void;
 };
 
 const MenuBtn: React.FC<MenuBtnProps> = ({ onClick }) => {
+   const buttonRef = useRef<HTMLButtonElement>(null);
+
+   const handleClick = () => {
+      onClick();
+      buttonRef.current?.classList.toggle("active");
+   };
+
    return (
-      <button type="button" onClick={onClick}>
-         Menu Btn
+      <button
+         title="settings"
+         className="menu-btn"
+         type="button"
+         onClick={handleClick}
+         ref={buttonRef}
+      >
+         <svg
+            version="1.1"
+            id="_x32_"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+         >
+            <style type="text/css"></style>
+            <g>
+               <rect className="st0" width="128" height="128" />
+               <rect x="192" className="st0" width="128" height="128" />
+               <rect x="384" className="st0" width="128" height="128" />
+               <rect y="192" className="st0" width="128" height="128" />
+               <rect x="192" y="192" className="st0" width="128" height="128" />
+               <rect x="384" y="192" className="st0" width="128" height="128" />
+               <rect y="384" className="st0" width="128" height="128" />
+               <rect x="192" y="384" className="st0" width="128" height="128" />
+               <rect x="384" y="384" className="st0" width="128" height="128" />
+            </g>
+         </svg>
       </button>
    );
 };

a45ddcb048e519c6c4da2eb21464e8a478f0f031
KrasimirKostadinov <krasimir.kostadinov@modis.com>
2023-03-21 11:35:31 +0200

    added Formik

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,27 +1,53 @@
-import { FormEvent } from "react";
+import { ErrorMessage, Field, Form, Formik, FormikHelpers } from "formik";
+import signupSchema, { SignupFormData } from "./validation.schema";
 import Button from "../../components/atoms/Button/Button";
-import Input from "../../components/atoms/Input/Input";
+import "../../components/atoms/Input/input.scss";
+
+const initialValues: SignupFormData = {
+   username: "",
+   password: "",
+};
 
 const Register: React.FC = () => {
-   const register = (e: FormEvent) => {
-      e.preventDefault();
-
-      console.log("Register me");
+   const register = (
+      values: SignupFormData,
+      actions: FormikHelpers<SignupFormData>
+   ) => {
+      console.log(values);
    };
 
    return (
       <main>
          <h1>Sign Up</h1>
-         <form onSubmit={register}>
-            <Input type="text" label="Username" name="username" />
-            <Input
-               type="password"
-               label="Password"
-               name="password"
-               className="mb-4"
-            />
-            <Button type="submit" text="Register" />
-         </form>
+         <Formik
+            initialValues={initialValues}
+            onSubmit={register}
+            validationSchema={signupSchema}
+         >
+            {() => (
+               <Form>
+                  <div className="input-wrapper">
+                     <label htmlFor="username">Username</label>
+                     <Field id="username" name="username" type="text" />
+                     <ErrorMessage
+                        component="div"
+                        name="username"
+                        className="error-message"
+                     />
+                  </div>
+                  <div className="input-wrapper">
+                     <label htmlFor="password">Password</label>
+                     <Field id="password" name="password" type="password" />
+                     <ErrorMessage
+                        component="div"
+                        name="password"
+                        className="error-message"
+                     />
+                  </div>
+                  <Button type="submit" text="Register" className="mt-3" />
+               </Form>
+            )}
+         </Formik>
       </main>
    );
 };
